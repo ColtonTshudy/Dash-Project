8491ae914161c899458e908f89d3e9ebf8d91eba
ColtonTshudy <coltont@vt.edu>
2023-04-07 23:07:45 -0400

    added gitignore

--- dash-react/src/App.js
+++ dash-react/src/App.js
@@ -1,32 +1,15 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
-import Speedometer from './components/speedMeter.js'
-import Currentmeter from './components/currentMeter.js'
-import NewMeter from './components/newMeter.js'
+import React from "react";
 
 
 function App() {
-  const [state, setState] = useState({ count: 0 })
-
-  useEffect(() => {
-    const timeId = setTimeout(() => {
-      setState(prevState => ({ count: (prevState.count + 0.1)%46 }))
-    }, 1)
-
-    return () => {
-      clearTimeout(timeId)
-    }
-  }, [state.count])
 
   return (
     <div className="center-screen">
-      <div className="viewport flex-container">
+      <div className="viewport flex-container rounded">
         <label>
           Moped Guage App
         </label>
-        <Speedometer id="speedometer" className="flex-1" value={state.count} label="Test" />
-        <Currentmeter id="battery-current" className="flex-1" value={state.count} label="Test" />
-        <Currentmeter id="battery-current" className="flex-1" value={state.count} label="Test" />
       </div>
     </div>
   );
