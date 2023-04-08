95933a17b2963c2d48cd2b47547a03e5a4da9725
ColtonTshudy <coltont@vt.edu>
2023-04-08 00:41:42 -0400

    basic sever and client

--- dash-react/src/App.js
+++ dash-react/src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 
 function App() {
 
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/members').then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
   return (
     <div className="center-screen">
-      <div className="viewport flex-container rounded">
+      <div className="viewport flex-container">
         <label>
           Moped Guage App
         </label>
+
+
+        {(typeof data.members === 'undefined') ? (
+          <p>loading...</p>
+        ) : (
+          data.members.map((member, i) => (
+            <p key={i}>{member}</p>
+          ))
+        )}
+
+
       </div>
     </div>
   );
---
+++ dash-react/src/components/plaintextreadout.js
@@ -0,0 +1,5 @@
+const PlaintextReadout = () => {
+
+}
+
+export default PlaintextReadout;
