33dc805da13675e3af7d6e1df048efa3b7ccca9a
ColtonTshudy <coltont@vt.edu>
2023-04-21 19:08:51 -0400

    socket not working

--- dash-react/src/App.js
+++ dash-react/src/App.js
@@ -1,36 +1,52 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
-//import { io } from "socket.io-client";
+import { io } from "socket.io-client";
 import LedReadout from './components/led-readout.js';
 //import Dial from './components/dial.js';
 import Speedometer from './components/speedMeter.js'
 // import ReactP5Comp from './components/react_p5.js';
 
 function App() {
-
+  const [socketInstance, setSocketInstance] = useState('')
   const [data, setData] = useState({})
   const [count, setCount] = useState({ count: 0 })
 
-  const url = "http://localhost:5001/can_data"
+  const url = "http://localhost:5001/"
+
+  // Handles setup of the socket object
+  useEffect(() => {
+    const socket = io(url, {
+      transports: ["websocket"],
+      cors: {
+        origin: "http://localhost:3000/",
+      },
+    });
+
+    setSocketInstance(socket);
+
+    socket.on('connect', (msg) => {
+      console.log(msg)
+    })
+    socket.on('get-data', (can_data) => {
+      setData(can_data)
+    })
+    socket.on('disconnect', (msg) => {
+      console.log(msg)
+    })
+
+    return () => {
+      socket.disconnect();
+    }
+  }, [])
 
   useEffect(() => {
-    fetch(url).then(res => {
-      if (res.status >= 400) {
-        throw new Error("Server responds with error!");
-      }
-      return res.json()
-    })
-      .then(
-        data => {
-          setData(data)
-        }
-      )
-      .catch(err => {
-        console.log(err)
-      })
+    if(socketInstance !== '')
+      socketInstance.emit('get-data')
+    
     const retryTimeId = setTimeout(() => {
       setCount(prevState => ({ count: prevState.count + 1 }))
     }, 10000) //retry every 10 seconds
+
     return () => {
       clearTimeout(retryTimeId) //reset retry timer
     }
@@ -43,10 +59,10 @@ function App() {
 
         {Object.entries(data).map(([key, value]) => <label key={key} className='rawtext'>{key} = {Math.trunc(value * 100) / 100}</label>)}
 
-        <LedReadout className="duty-readout flex-center" value={data.duty_cycle} max={1}/>
+        <LedReadout className="duty-readout flex-center" value={data.duty_cycle} max={1} />
         {/* <Dial className="speed-dial flex-center" value={data.mph} max={45}/> */}
         {/* <ReactP5Comp className="item2" /> */}
-        <Speedometer id="test" className="speed-dial flex-center" value={data.mph}/>
+        <Speedometer id="test" className="speed-dial flex-center" value={data.mph} />
 
       </div>
     </div>
--- python-backend/server.py
+++ python-backend/server.py
@@ -7,10 +7,13 @@ __status__ = "Prototyping"
 
 from flask import Flask, Response, request
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 
 # FLASK SERVER
 app = Flask(__name__)
-CORS(app)
+app.config['SECRET_KEY'] = 'secret'
+CORS(app, resources={r"/*": {"origins": "*"}})
+socket = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route("/can_data")
@@ -20,11 +23,31 @@ def can_data():
 
 
 @app.route('/', methods=['POST'])
-def get_data():
+def store_data():
     global can_data_dict
     can_data_dict = request.json
     print(f'Recieved from client: {can_data_dict}')
     return Response('success')
 
 
-app.run(debug=True, port=5001, threaded=True)
+@socket.on("connect")
+def connected():
+    print(request.sid)
+    print('client has connected')
+    emit("connect", {'data': f'id: {request.sid} is connected'})
+
+
+@socket.on('get_data')
+def send_data():
+    global can_data_dict
+    emit('data', can_data_dict, broadcast=True)
+
+
+@socket.on('disconnect')
+def disconnected():
+    print("user disconnected")
+    emit("disconnect", f"user {request.sid} disconnected", broadcast=True)
+
+
+if __name__ == '__main__':
+    socket.run(app, debug=True, port=5001)
