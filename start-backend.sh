fe01766ef9c25790aa8da198ae8c8ea98b3f0da8
ColtonTshudy <coltont@vt.edu>
2023-05-10 17:11:30 -0400

    Changed frontend and backend sh scripts

--- start-backend.sh
+++ start-backend.sh
@@ -1,5 +1,4 @@
 sudo -H -u tomos /bin/python /home/tomos/Projects/Dash/Dash-Project/python-backend/main.py &
 sudo -H -u tomos /bin/python /home/tomos/Projects/Dash/Dash-Project/python-backend/server.py &
-(cd /home/tomos/Projects/Dash/Dash-Project/dash-react/build ;sudo -H -u tomos /bin/python -m http.server 5051) &
 
 exit 0
--- start-frontend.sh
+++ start-frontend.sh
@@ -1,1 +1,2 @@
-DISPLAY=:0 chromium-browser --kiosk http://localhost:5051
+(cd /home/tomos/Projects/Dash/Dash-Project/dash-react/build ;sudo -H -u tomos /bin/python -m http.server 5051) &
+DISPLAY=:0 firefox --kiosk http://localhost:5051
